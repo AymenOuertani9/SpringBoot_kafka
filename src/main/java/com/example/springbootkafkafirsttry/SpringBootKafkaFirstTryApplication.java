3b1668d6e964bc0ec2c5738b1f14e0a7522e27db
AymenOuertani9 <aymen.ouertani@esprt.tn>
2023-07-25 21:52:52 +0100

    first commit

---
+++ src/main/java/com/example/springbootkafkafirsttry/SpringBootKafkaFirstTryApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkafirsttry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaFirstTryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaFirstTryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootkafkafirsttry/SpringBootKafkaFirstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkafkafirsttry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaFirstTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
